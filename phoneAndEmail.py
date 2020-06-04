352a1c48a78a14d5bafc58b4e94d5a0a67417941
Mikhail Aleksandrov <al-mn@yandex.ru>
2020-06-04 12:59:43 +0300

    first commit

--- phoneAndEmail.py
+++ phoneAndEmail.py
@@ -8,7 +8,7 @@ import pyperclip, re
 phoneRegex = re.compile(r'''(
     (\d{3}|\(\d{3}\))?              # area code 
     (\s|-|\.)?                      # separator
-    (\d{3})                           # first 3 digits
+    (\d{3})                         # first 3 digits
     (\s|-|\.)                       # separator
     (\d{4})                           # last 4 digits
     (\s*(ext|x|ext.)\s*(\d{0,5}))?    # extension
