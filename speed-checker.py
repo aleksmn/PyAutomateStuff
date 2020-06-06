ac764e0bbbd36e7d6405f1b6160124edd0926f81
Mikhail Aleksandrov <al-mn@yandex.ru>
2020-06-06 16:06:58 +0300

    add writing to csv

--- speed-checker.py
+++ speed-checker.py
@@ -1,6 +1,9 @@
 import speedtest
 import time
 import datetime
+import csv
+
+print("Начинаем проверку скорости интернета...")
 
 speedtester = speedtest.Speedtest()
 best_server = speedtester.get_best_server()
@@ -10,6 +13,15 @@ now = datetime.datetime.now()
 now_date = now.strftime("%d.%m.%Y")
 now_time = now.strftime("%H:%M")
 
+measurement = [now_date, now_time, download_speed]
+path_to_csv = '/home/mikhail/Desktop/speed-check.csv'
+
+with open(path_to_csv, 'a', newline='') as csvfile:
+    writer = csv.writer(csvfile, delimiter = ';')
+    writer.writerow(measurement)
+    
 print("Дата:", now_date)
 print("Время:", now_time)
 print(f'Скорость загрузки: {download_speed} Мбит/с')
+print("Результаты записаны в файл", path_to_csv)
+
