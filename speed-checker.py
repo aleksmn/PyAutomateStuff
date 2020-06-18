f269b902c6fc05ac1a7492736005ac057920145a
Mikhail Aleksandrov <al-mn@yandex.ru>
2020-06-18 08:18:15 +0300

    add exception

--- speed-checker.py
+++ speed-checker.py
@@ -3,25 +3,29 @@ import time
 import datetime
 import csv
 
-print("Начинаем проверку скорости интернета...")
+try:   
+    print("Запускаем Speedtest...")
+    speedtester = speedtest.Speedtest()
+    print("Начинаем проверку скорости интернета...")
+    best_server = speedtester.get_best_server()
+    download_speed = round(speedtester.download() / 1000000, 2)
 
-speedtester = speedtest.Speedtest()
-best_server = speedtester.get_best_server()
-download_speed = round(speedtester.download() / 1000000, 2)
+    now = datetime.datetime.now()
+    now_date = now.strftime("%d.%m.%Y")
+    now_time = now.strftime("%H:%M")
 
-now = datetime.datetime.now()
-now_date = now.strftime("%d.%m.%Y")
-now_time = now.strftime("%H:%M")
+    measurement = [now_date, now_time, download_speed]
+    path_to_csv = '/home/mikhail/Desktop/speed-check.csv'
 
-measurement = [now_date, now_time, download_speed]
-path_to_csv = '/home/mikhail/Desktop/speed-check.csv'
+    with open(path_to_csv, 'a', newline='') as csvfile:
+        writer = csv.writer(csvfile, delimiter = ';')
+        writer.writerow(measurement)
+        
+    # print("Дата:", now_date)
+    print("Время измерения:", now_time)
+    print(f'Скорость загрузки: {download_speed} Мбит/с')
+    print("Результаты записаны в файл", path_to_csv)
+    print('')
 
-with open(path_to_csv, 'a', newline='') as csvfile:
-    writer = csv.writer(csvfile, delimiter = ';')
-    writer.writerow(measurement)
-    
-print("Дата:", now_date)
-print("Время:", now_time)
-print(f'Скорость загрузки: {download_speed} Мбит/с')
-print("Результаты записаны в файл", path_to_csv)
-
+except speedtest.ConfigRetrievalError as error:
+    print('Измерение не выполнено. Нет соединения с интернетом.')
